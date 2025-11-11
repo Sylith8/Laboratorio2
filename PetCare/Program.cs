eb95672e46b4786d7fd3fe0c45e84532ddb71ad3
Isaac M <isaackraz@gmail.com>
2025-11-10 22:50:23 -0600

    Codigo final y funcional

--- PetCare/Program.cs
+++ PetCare/Program.cs
@@ -1,15 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Agrega soporte para controladores y vistas
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuraci�n del pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,8 +19,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Accede a /Mascota/Registrar
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Mascota}/{action=Index}/{id?}");
 
 app.Run();
